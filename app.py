f5d8833e5b793b9d27b4b039c774f6ee73e462bd
Vaibhav Singh <88562515+NewEthicker@users.noreply.github.com>
2025-08-09 12:50:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,5 +48,5 @@ def toggle():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8080)
